13c2ae8a820005cb2bba89456523f0e812e48fa9
pchrisoc <pchrisoc@berkeley.edu>
2025-02-25 12:55:21 -0800

    about

--- src/components/Sections/About.tsx
+++ src/components/Sections/About.tsx
@@ -1,6 +1,6 @@
-import { FC, memo } from 'react';
+import {FC, memo} from 'react';
 
-import { aboutData, SectionId } from '../../data/data';
+import {aboutData, SectionId} from '../../data/data';
 import Section from '../Layout/Section';
 
 const About: FC = memo(() => {
